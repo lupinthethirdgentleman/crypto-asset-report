2c473e7e6e9df16a7ddb091307ddc9a7f981831f
lupinthethirdgentleman <lupinthethirdgentleman@gmail.com>
2024-02-29 13:03:45 +0300

    resolution

--- next.config.js
+++ next.config.js
@@ -4,14 +4,7 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 
 // Remove this if you're not using Fullcalendar features
-const withTM = require('next-transpile-modules')([
-  '@fullcalendar/common',
-  '@fullcalendar/react',
-  '@fullcalendar/daygrid',
-  '@fullcalendar/list',
-  '@fullcalendar/timegrid',
-  'react-ts-tradingview-widgets'
-])
+const withTM = require('next-transpile-modules')(['react-ts-tradingview-widgets'])
 
 module.exports = withTM({
   trailingSlash: true,
