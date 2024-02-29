106bc8e52370857530319e854ab8104187a210d8
lupinthethirdgentleman <lupinthethirdgentleman@gmail.com>
2024-02-29 12:09:40 +0300

    config update

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path')
 
 /** @type {import('next').NextConfig} */
 
 // Remove this if you're not using Fullcalendar features
-const withTM = require('next-transpile-modules')([
-  '@fullcalendar/common',
-  '@fullcalendar/react',
-  '@fullcalendar/daygrid',
-  '@fullcalendar/list',
-  '@fullcalendar/timegrid',
-  'react-ts-tradingview-widgets'
-])
+const withTM = require('next-transpile-modules')(['react-ts-tradingview-widgets'])
 
 module.exports = withTM({
   trailingSlash: true,
@@ -21,8 +13,7 @@ module.exports = withTM({
   },
   webpack: config => {
     config.resolve.alias = {
-      ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+      ...config.resolve.alias
     }
 
     return config
